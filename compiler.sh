ae91e9d20b9bfbdc1fa01a7f695c2bd1a6232fa0
fengjijiao <16521693+fengjijiao@users.noreply.github.com>
2021-01-26 07:52:53 +0800

    update compiler file

--- compiler.sh
+++ compiler.sh
@@ -5,9 +5,9 @@ compiler() {
   else
     suffix="";
   fi
-  CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -o bin/tlssocks-${1}-${2}${suffix} cmd/tlssocks/tlssocks.go
+  CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -o bin/xtlssocks-${1}-${2}${suffix} cmd/xtlssocks/xtlssocks.go
   CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -o bin/tcpproxy-${1}-${2}${suffix} cmd/tcpproxy/tcpproxy.go
-  CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -trimpath -o bin/tlssocksproxy-${1}-${2}${suffix} cmd/tlssocksproxy/tlssocksproxy.go
+  CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -trimpath -o bin/xtlssocksproxy-${1}-${2}${suffix} cmd/xtlssocksproxy/xtlssocksproxy.go
   CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -o bin/socksclient-${1}-${2}${suffix} cmd/socksclient/socksclient.go
 }
 compiler linux amd64
